e23ba760fc2fa7b22926b17838c8ced6caf340c9
delude88 <tobias.hegemann@googlemail.com>
2020-09-15 15:07:30 +0200

    fixed untracked files

--- dist/server/src/auth/Authentication.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authentication = void 0;
-const GoogleAuthentication_1 = require("./google/GoogleAuthentication");
-exports.authentication = new GoogleAuthentication_1.default();
-//# sourceMappingURL=Authentication.js.map
