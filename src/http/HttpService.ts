6d432edf818371acc775dac51211083cee0ae53e
delude88 <tobias.hegemann@googlemail.com>
2020-10-06 16:14:00 +0200

    Temp debug output

--- src/http/HttpService.ts
+++ src/http/HttpService.ts
@@ -13,8 +13,36 @@ class HttpServer {
         this.database = database;
     }
 
-    init(app: core.Express) {
+    handleProducerRequest(req, res) {
+        if (
+            !req.params.id
+            || typeof req.params.id !== 'string'
+        ) {
+            return res.sendStatus(400);
+        }
+        return this.authentication.authorizeRequest(req)
+            .then(async () => {
+                let producer = await this.database.readVideoProducer(req.params.id).catch(error => console.error(error));
+                if (!producer) {
+                    producer = await this.database.readAudioProducer(req.params.id);
+                }
+                if (producer) {
+                    return res.status(200).json(producer);
+                } else {
+                    console.log("Was looking for " + req.params.id);
+                    console.log("But only found following:");
+                    await this.database.db().collection("videoproducers").find({}).toArray()
+                        .then(producers => producers.map(producer => console.log(producer)))
+                }
+                return res.sendStatus(404);
+            })
+            .catch((error) => {
+                console.log(error);
+                return res.sendStatus(401);
+            });
+    }
 
+    init(app: core.Express) {
         app.use(expressPino());
 
         app.get('/beat', function (req, res) {
@@ -22,34 +50,10 @@ class HttpServer {
         });
 
         // GET SPECIFIC PUBLIC PRODUCER
-        app.get('/producers/:id', function (req, res) {
-            if (
-                !req.params.id
-                || typeof req.params.id !== 'string'
-            ) {
-                return res.sendStatus(400);
-            }
-            return this.authentication.authorizeRequest(req)
-                .then(async () => {
-                    let producer = await this.database.readVideoProducer(req.params.id).catch(error => console.error(error));
-                    if (!producer) {
-                        producer = await this.database.readAudioProducer(req.params.id);
-                    }
-                    if (producer) {
-                        return res.status(200).json(producer);
-                    } else {
-                        console.log("Was looking for " + req.params.id);
-                        console.log("But only found following:");
-                        await this.database.db().collection("videoproducers").find({}).toArray()
-                            .then(producers => producers.map(producer => console.log(producer)))
-                    }
-                    return res.sendStatus(404);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    return res.sendStatus(401);
-                });
+        app.get('/producers/:id', (req, res) => {
+            return this.handleProducerRequest(req, res);
         });
     }
 }
+
 export default HttpService;
